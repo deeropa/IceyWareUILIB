3787a0a1081a49e1066ca16f9e4b34402b3931e6
deeropa <idiotasdomas@gmail.com>
2026-03-02 08:19:41 +0000

    Adjust main frame position in iceyware.lua

--- iceyware.lua
+++ iceyware.lua
@@ -158,7 +158,7 @@ function Library:CreateWindow(title, toggleKey)
     main.Name = "MainFrame"
     main.BackgroundColor3 = C.mainBg
     main.BorderSizePixel = 0
-    main.Position = UDim2.new(0, 0, 0, TH + 3)
+    main.Position = UDim2.new(0, 0, 0, TH + 2)
     main.Size = UDim2.new(1, 0, 0, 326)
     main.Parent = wrapper
     corner(main)
